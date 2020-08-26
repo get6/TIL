ea3e4ea0e1debd74e0e9062a57077038955d51c6
get6 <ittae.com@gmail.com>
2020-08-26 21:26:42 +0900

    file 생성

--- firebase_auth_demo_flutter/lib/services/apple_sign_in_available.dart
+++ firebase_auth_demo_flutter/lib/services/apple_sign_in_available.dart
@@ -1,3 +1,12 @@
-class AppleSignInAvailable {
+import 'package:apple_sign_in/apple_sign_in.dart';
 
+class AppleSignInAvailable {
+  AppleSignInAvailable(this.isAvailable);
+
+  final bool isAvailable;
+
+  static Future<AppleSignInAvailable> check() async {
+    bool isAvailable = await AppleSignIn.isAvailable();
+    return AppleSignInAvailable(isAvailable);
+  }
 }
