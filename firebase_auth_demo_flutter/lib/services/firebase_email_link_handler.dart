8180f84ea49f2ff7f7925b27c600314168d45605
get6 <ittae.com@gmail.com>
2020-09-01 23:55:55 +0900

    firebase_email_link_handler.dart 작업 중

---
+++ firebase_auth_demo_flutter/lib/services/firebase_email_link_handler.dart
@@ -0,0 +1,92 @@
+import 'package:firebase_auth_demo_flutter/constants/strings.dart';
+import 'package:firebase_auth_demo_flutter/services/auth_service.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rxdart/rxdart.dart';
+
+import 'email_secure_store.dart';
+
+enum EmailLinkErrorType {
+  linkError,
+  isNotSignInWithEmailLink,
+  emailNotSet,
+  signInFailed,
+  userAlreadySignedIn,
+}
+
+class EmailLinkError {
+  EmailLinkError({@required this.error, this.description});
+
+  final EmailLinkErrorType error;
+  final String description;
+
+  Map<EmailLinkErrorType, String> get _messages => {
+        EmailLinkErrorType.linkError: description,
+        EmailLinkErrorType.isNotSignInWithEmailLink:
+            Strings.isNotSignInWithEmailLinkMessage,
+        EmailLinkErrorType.emailNotSet: Strings.submitEmailAgain,
+        EmailLinkErrorType.signInFailed: description,
+        EmailLinkErrorType.userAlreadySignedIn: Strings.userAlreadySignedIn,
+      };
+
+  String get message => _messages[error];
+
+  @override
+  String toString() => '$error: ${_messages[error]}';
+
+  @override
+  int get hashCode => error.hashCode;
+
+  @override
+  bool operator ==(Object other) {
+    if (other is EmailLinkError) {
+      return error == other.error && description == other.description;
+    }
+    return false;
+  }
+}
+
+// Checks incoming dynamic links and uses them to sign in the user with Firebase
+class FirebaseEmailLinkHandler {
+  FirebaseEmailLinkHandler({
+    @required this.auth,
+    @required this.emailStore,
+    @required this.firebaseDynamicLinks,
+  });
+
+  final AuthService auth;
+  final EmailSecureStore emailStore;
+  final FirebaseDynamicLinks firebaseDynamicLinks;
+
+  // Sets up listeners to process all links from [FirebaseDynamicLinks.instance.getIntialLink()] and [FirebaseDynamicLinks.instance.onLink]
+  Future<void> init() async {
+    try {
+      // Listen to incoming links when the app is open
+      firebaseDynamicLinks.onLink(
+        onSuccess: (linkData) => _processDynamicLink(linkData?.link),
+        onError: (error) => _handleLinkError(PlatformException(
+            code: error.code, message: error.message, details: error.details)),
+      );
+
+      // Check dynamic link once on app startup.
+      // This is required to process any dynamic links that are opened when the app was closed
+      final linkData = await firebaseDynamicLinks.getInitialLink();
+      final link = linkData?.link?.toString();
+      if (link != null) {
+        await _processDynamicLink(linkData?.link);
+      }
+    } on PlatformException catch (e) {
+      _handleLinkError(e);
+    }
+  }
+
+  // Clients can listen to this stream and show error alerts when dynamic link processing fails
+  final PublishSubject<EmailLinkError> _errorController =
+      PublishSubject<EmailLinkError>();
+
+  Stream<EmailLinkError> get errorStream => _errorController.stream;
+
+  // Clients can listen to this stream and show a loading indicator while sign in is in progress
+  final ValueNotifier<bool> isLoading = ValueNotifier<bool>(false);
+}
