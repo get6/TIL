03495f9a7bed0e7ef4b45b4cfc199edd47a0ca6a
get6 <ittae.com@gmail.com>
2020-09-10 09:48:12 +0900

    create flutter

--- firebase_auth_demo_flutter/lib/main.dart
+++ firebase_auth_demo_flutter/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth_demo_flutter/app/email_link_error_presenter.dart';
 import 'package:firebase_auth_demo_flutter/app/auth_widget.dart';
 import 'package:firebase_auth_demo_flutter/services/apple_sign_in_available.dart';
 import 'package:firebase_auth_demo_flutter/services/auth_service.dart';
+import 'package:firebase_auth_demo_flutter/services/auth_service_adapter.dart';
 import 'package:firebase_auth_demo_flutter/services/firebase_email_link_handler.dart';
 import 'package:firebase_auth_demo_flutter/services/email_secure_store.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
@@ -10,16 +11,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:provider/provider.dart';
 
-import 'app/auth_widget.dart';
-import 'services/auth_service_adapter.dart';
-
 Future<void> main() async {
+  // Fix for: Unhandled Exception: ServicesBinding.defaultBinaryMessenger was accessed before the binding was initialized.
   WidgetsFlutterBinding.ensureInitialized();
   final appleSignInAvailable = await AppleSignInAvailable.check();
   runApp(MyApp(appleSignInAvailable: appleSignInAvailable));
 }
 
 class MyApp extends StatelessWidget {
+  // [initialAuthServiceType] is made configurable for testing
   const MyApp(
       {this.initialAuthServiceType = AuthServiceType.firebase,
       this.appleSignInAvailable});
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // MultiProvider for top-level services that can be created right away
     return MultiProvider(
       providers: [
         Provider<AppleSignInAvailable>.value(value: appleSignInAvailable),
@@ -38,30 +39,30 @@ class MyApp extends StatelessWidget {
           dispose: (_, AuthService authService) => authService.dispose(),
         ),
         Provider<EmailSecureStore>(
-          create: (_) =>
-              EmailSecureStore(flutterSecureStorage: FlutterSecureStorage()),
+          create: (_) => EmailSecureStore(
+            flutterSecureStorage: FlutterSecureStorage(),
+          ),
         ),
         ProxyProvider2<AuthService, EmailSecureStore, FirebaseEmailLinkHandler>(
           update: (_, AuthService authService, EmailSecureStore storage, __) =>
               FirebaseEmailLinkHandler(
-                  auth: authService,
-                  emailStore: storage,
-                  firebaseDynamicLinks: FirebaseDynamicLinks.instance)
-                ..init(),
+            auth: authService,
+            emailStore: storage,
+            firebaseDynamicLinks: FirebaseDynamicLinks.instance,
+          )..init(),
           dispose: (_, linkHandler) => linkHandler.dispose(),
-        )
+        ),
       ],
       child: AuthWidgetBuilder(
-        builder: (BuildContext context, AsyncSnapshot<User> userSnapshot) {
-          return MaterialApp(
-            theme: ThemeData(primarySwatch: Colors.indigo),
-            home: EmailLinkErrorPresenter.create(
-              context,
-              child: AuthWidget(userSnapshot: userSnapshot),
-            ),
-          );
-        },
-      ),
+          builder: (BuildContext context, AsyncSnapshot<User> userSnapshot) {
+        return MaterialApp(
+          theme: ThemeData(primarySwatch: Colors.indigo),
+          home: EmailLinkErrorPresenter.create(
+            context,
+            child: AuthWidget(userSnapshot: userSnapshot),
+          ),
+        );
+      }),
     );
   }
 }
