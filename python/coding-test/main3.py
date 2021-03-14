9bbb3913be1957ee95bc196cf35a1ff22845e3d2
get6 <ittae.com@gmail.com>
2021-03-14 21:00:41 +0900

    정렬 보는 중

--- python/coding-test/main3.py
+++ python/coding-test/main3.py
@@ -346,38 +346,189 @@ def prime_number():
             print(i)
 
 
-# 642 정답 10, 643 정답 11
-x = int(input())
-cnt = 0
-# while True:
-#     if x == 1:
-#         break
-#     isWhile = False
-#     while (x - 1) % 3 == 0:
-#         x -= 1
-#         x //= 3
-#         cnt += 2
-#         isWhile = True
-#     while x % 3 == 0:
-#         x //= 3
-#         cnt += 1
-#         isWhile = True
-#     while x % 2 == 0:
-#         x //= 2
-#         cnt += 1
-#         isWhile = True
-#     if not isWhile:
-#         x -= 1
-#         cnt += 1
+def getNumCount():
+    # 642 정답 10, 643 정답 11
+    x = int(input())
+    # 입력된 값 + 1의 값으로 배열을 생성 = 0부터 시작하기 때문에
+    dp = [0 for _ in range(x + 1)]
+    # 다이나믹 프로그래밍 참고하기
+    for i in range(2, x + 1):
+        # 2, 3으로 나눠지는 지 값을 확인하기 전에 모르기 때문에 기본적으로 이전 값을 현재 위치에 넣어줌
+        dp[i] = dp[i - 1] + 1
+        # 2로 나눠지고 현재 값이 i를 2로 나눈 위치의 값 + 1보다 클 때에만 나눔.
+        # 현재값이 클 때에만 하는 이유는 이전값이 크거나 같다면 이후 값에 영향을 미친다.
+        # 94는 실제로 7 인데 i % 2 == 0로만 하면 10임.
+        if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
+            dp[i] = dp[i // 2] + 1
+        if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
+            dp[i] = dp[i // 3] + 1
 
-dp = [0 for _ in range(x + 1)]
-# 다이나믹 프로그래밍 참고하기
-for i in range(2, x + 1):
-    dp[i] = dp[i - 1] + 1
+    print(dp[x])
 
-    if i % 2 == 0 and dp[i] > dp[i // 2] + 1:
-        dp[i] = dp[i // 2] + 1
-    if i % 3 == 0 and dp[i] > dp[i // 3] + 1:
-        dp[i] = dp[i // 3] + 1
 
-print(dp[x])
+# DFS 메소드 정의
+def dfs(graph, v, visited):
+    # 현재 노드를 방문 처리
+    visited[v] = True
+    print(v, end=" ")
+    # 현재 노드와 연결된 다른 노드를 재귀적으로 방문
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * len(graph)
+# 정의된 DFS 함수 호출
+# dfs(graph, 1, visited)
+
+from collections import deque
+
+
+# BFS 메소드 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=" ")
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+# 정의된 BFS 함수 호출
+# bfs(graph, 1, visited)
+
+def solution(mylist):
+    answer = []
+    # for i in mylist:
+    #     answer.append(int(i))
+    answer = list(map(int, mylist))
+
+    return answer
+
+
+# print(solution(['1', '100', '33']))
+
+def lunarProj():
+    # 테스트케이스의 개수
+    t = int(input())
+    answer = []
+    while t > 0:
+        # 출발 지점 x, 도착 지점 y까지 필요한 최소한의 공간이동 장치 작동 횟수
+        x, y = map(int, input().split())
+        if x == 0:
+            x = 1
+        length = y - x
+        arr = [0] * length
+
+        for i in range(1, length):
+            arr[i] = arr[i - 1] + 1
+
+        answer.append(arr[-1] + 1)
+        t -= 1
+
+    for i in answer:
+        print(i)
+
+
+def findIce():
+    # n, m을 공백으로 구분하여 입력받기
+    n, m = map(int, input().split())
+
+    # 2차원 리스트의 맵 정보 입력받기
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input())))
+
+    # DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+    def dfs(x, y):
+        # 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if x <= -1 or x >= n or y <= -1 or y >= m:
+            return False
+        # 현재 노드를 아직 방문하지 않았다면
+        if graph[x][y] == 0:
+            # 해당 노드 방문 처리
+            graph[x][y] = 1
+            # 상,좌,하,우의 위치도 재귀적으로 호출
+            dfs(x - 1, y)
+            dfs(x, y - 1)
+            dfs(x + 1, y)
+            dfs(x, y + 1)
+            return True
+        return False
+
+    # 모든 노드(위치)에 대하여 음료수 채우기
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            # 현재 위치에서 DFS 수행
+            if dfs(i, j):
+                result += 1
+
+    print(result)
+
+
+from collections import deque
+
+# N, M을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+# 이동할 네 방향 정의(상, 하, 좌, 우)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+# BFS 소스코드 구현
+def bfs(x, y):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque()
+    queue.append((x, y))
+    # 큐가 빌 때까지 반복
+    while queue:
+        x, y = queue.popleft()
+        # 현재 위치에서 네 방향으로의 위치 확인
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 미로 찾기 공간을 벗어난 경우 무시
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            # 벽인 경우 무시
+            if graph[nx][ny] == 0:
+                continue
+            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    # 가장 오른쪽 아래까지의 최단 거리 반환
+    print(graph)
+    return graph[n - 1][m - 1]
+
+
+# BFS를 수행한 결과 출력
+# print(bfs(0, 0))
