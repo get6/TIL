4f65d2154543f0ac452ca589c9b1e2482e464606
get6 <ittae.com@gmail.com>
2021-06-12 18:42:09 +0900

    Add Python code

---
+++ python/codeit/python_intro/problem5.py
@@ -0,0 +1,7 @@
+input_string = input("Input String : ")
+find_string = input("Find String : ")
+replace_string = input("Replace String : ")
+
+result = input_string.replace(find_string, replace_string)
+print(f'Result : {result}')
+
